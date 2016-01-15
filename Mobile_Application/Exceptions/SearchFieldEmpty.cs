c4569fd6b1adb058badf08e5fa5c95f98551c340
Eisaltar <xav314@gmail.com>
2016-01-15 23:02:42 +0100

    final

---
+++ Mobile_Application/Exceptions/SearchFieldEmpty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Application.Exceptions
+{
+
+    class SearchFieldEmpty : Exception
+    {
+        public string Explain { get; set; }
+
+        public SearchFieldEmpty(string explain)
+        {
+            this.Explain = explain;
+        }
+
+        public string toString()
+        {
+            return Explain;
+        }
+    }
+}
